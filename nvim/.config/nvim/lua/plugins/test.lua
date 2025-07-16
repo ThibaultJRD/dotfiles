49c904d25ddb0c0b9f3d63071e6a1c8646df0c35
Thibault Jaillard <thibault.jaillard@gmail.com>
2025-07-15 22:11:37 -0400

    fix: remove neotest-go for now

--- nvim/.config/nvim/lua/plugins/test.lua
+++ nvim/.config/nvim/lua/plugins/test.lua
@@ -1,13 +1,11 @@
 return {
   "nvim-neotest/neotest",
   dependencies = {
-    "nvim-neotest/neotest-go",
     "marilari88/neotest-vitest",
     "nvim-neotest/neotest-jest",
   },
   opts = {
     adapters = {
-      ["neotest-go"] = {},
       ["neotest-vitest"] = {},
       ["neotest-jest"] = {
         jestCommand = "npm test --",
