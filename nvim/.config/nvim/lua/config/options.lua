a2f91c692ce493f73ede3367610065e77517d66e
Thibault Jaillard <thibault.jaillard@gmail.com>
2025-08-04 10:17:27 -0400

    chore: add options for prettier and eslint

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,3 +3,10 @@
 -- Add any additional options here
 
 vim.g.maplocalleader = "ù"
+
+-- Enable the option to require a Prettier config file
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = true
+
+-- Set to false to disable auto format
+vim.g.lazyvim_eslint_auto_format = true
